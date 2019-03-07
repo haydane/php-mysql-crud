d88e3e8e11d413c83a14de58936de786669ac3f3
danehay <haydane9@gmail.com>
2019-03-07 16:55:39 +0700

    created delete users and change table style

--- controller/process.php
+++ controller/process.php
@@ -1,5 +1,11 @@
 <?php
   $con = mysqli_connect("127.0.0.1","root","password","web");
+  $update = false;
+  $name = '';
+  $email = '';
+  $phone = '';
+  $id = 0;
+  $exist = false;
 
   session_start();
 
@@ -8,15 +14,25 @@
   }
   if(isset($_POST['insert']))
   {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    mysqli_query($con,"insert into users(name,email,phone) values('$name','$email','$phone')") or die($con->mysqli->connect_error);
-
-    $_SESSION['message'] = "Record has been saved";
-    $_SESSION['msg_type'] = "success";
-
-    header("location: ../index.php");
+    $name = $_POST['name'].trim();
+    $email = $_POST['email'].trim();
+    $phone = $_POST['phone'].trim();
+    $res = mysqli_query($con,"SELECT name FROM users WHERE name='$name'") or die(`error: ${$con->mysqli_error}`);
+    $row = $res->fetch_array();
+    echo count($row['name']);
+    if(count($row['name'])==1)
+    {
+      $exist = true;
+      $_SESSION['message'] = "Your name is existed!";
+      $_SESSION['msg_type'] = "info";
+      header("location: ../index.php");
+    }
+    else{
+      mysqli_query($con,"insert into users(name,email,phone) values('$name','$email','$phone')") or die($con->mysqli->connect_error);
+      $_SESSION['message'] = "Record has been saved";
+      $_SESSION['msg_type'] = "success";
+      header("location: ../index.php");
+    }
   }
 
   if(isset($_GET['delete']))
@@ -31,6 +47,7 @@
 
   if(isset($_GET['edit']))
   {
+    $update = true;
     $id = $_GET['edit'];
     $res = mysqli_query($con,"SELECT * FROM users WHERE id=$id") or die($con->mysqli_error);
     if(count($res)==1){
@@ -39,6 +56,20 @@
       $email = $row['email'];
       $phone = $row['phone'];
     }
+  }
+
+  if(isset($_POST['update']))
+  {
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+
+    mysqli_query($con,"UPDATE users SET name='$name',email='$email',phone='$phone' WHERE id=$id") or die($con->mysqli_error);
+    header("location: ../index.php");
+    $_SESSION['message'] = "Record has been updated";
+    $_SESSION['msg_type'] = "warning";
+
   }
 
 ?>
--- index.php
+++ index.php
@@ -18,26 +18,34 @@
     <div class="container">
       <?php include_once './controller/process.php'; ?>
       <?php if(isset($_SESSION['message'])):  ?>
-        <div class="alert alert-<?=$_SESSION['msg_type'] ?>">
-          <?php 
+      <div class="alert alert-<?=$_SESSION['msg_type'] ?>">
+        <?php 
             echo $_SESSION['message']; 
             unset($_SESSION['message']); 
           ?>
-        </div> 
+      </div>
       <?php endif ?>
+      <?php if($exist==true): ?>
+      <div class="alert alert-<?=$_SESSION['msg_type'] ?>">
+        <?php 
+              echo $_SESSION['message']; 
+              unset($_SESSION['message']); 
+            ?>
+      </div>
+      <?php endif; ?>
 
       <?php 
-        $res = mysqli_query($con,"SELECT * FROM users");
+        $res = mysqli_query($con,"SELECT * FROM users order by id desc");
         if($res->num_rows>0){
         }
         else{
           echo "no result";
         }
       ?>
-      
 
-      <div class="row justify-content-center">
-        <table class="table">
+
+      <div class="table-wrapper-scroll-y" style="height: 300px; overflow-y: scroll;">
+        <table class="table table-bordered table-striped mb-0">
           <thead>
             <tr>
               <th>Name</th>
@@ -55,29 +63,48 @@
               <td><?php echo $row['phone'];  ?></td>
               <td>
                 <a class="btn btn-info" name="edit" href="index.php?edit=<?php echo $row['id']; ?>">Edit</a>
-                <a class="btn btn-danger" name="delete" href="./controller/process.php?delete=<?php echo $row['id']; ?>">Delete</a>
-            </td>
+                <a class="btn btn-danger" name="delete"
+                  href="./controller/process.php?delete=<?php echo $row['id']; ?>">Delete</a>
+              </td>
             </tr>
             <?php endwhile; ?>
 
           </tbody>
         </table>
       </div>
+      <br>
 
       <form action="./controller/process.php" method="POST">
+        <input type="hidden" name="id" value="<?php echo $id; ?>">
         <div class="form-group">
           <label>Name</label>
-          <input type="text" id="name" name="name" value="<?php echo $name; ?>" class="form-control" placeholder="name">
+          <input type="text" id="name" required name="name" value="<?php echo $name; ?>" class="form-control"
+            placeholder="name">
         </div>
         <div class="form-group">
           <label>Email</label>
-          <input type="email" id="email" name="email" value="<?php echo $email; ?>" class="form-control" placeholder="email">
+          <input type="email" id="email" name="email" value="<?php echo $email; ?>" class="form-control"
+            placeholder="email">
         </div>
         <div class="form-group">
           <label>Phone</label>
-          <input type="text" id="phone" name="phone" value="<?php echo $phone; ?>"  class="form-control" placeholder="phone">
+          <input type="number" id="phone" name="phone" value="<?php echo $phone; ?>" class="form-control"
+            placeholder="phone">
         </div>
-        <input type="submit" name="insert" value="Add New User" class="btn btn-primary">
+
+        <div class="form-group">
+          <?php 
+          if($update == true): 
+        ?>
+          <input type="submit" name="update" value="Update" class="btn btn-info">
+          <?php 
+          else: 
+        ?>
+          <input type="submit" name="insert" value="Add New User" class="btn btn-primary">
+          <?php 
+          endif; 
+        ?>
+        </div>
       </form>
     </div>
   </div>
